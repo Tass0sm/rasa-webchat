5a2402fde70c06625dd67480f875f460f694eaec
Tassos Manganaris <tassos.manganaris@gmail.com>
2022-10-13 14:09:20 -0400

    Update webpack, sass packages. Simplify webpack config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,93 +1,39 @@
-
-
-const path = require('path');
+const path = require("path");
 // eslint-disable-next-line import/no-extraneous-dependencies
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const { version } = require('./package.json');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { version } = require("./package.json");
 
-module.exports = [{
-  // entry: ['babel-polyfill', './index.js'],
-  entry: './umd.js',
-  output: {
-    path: path.join(__dirname, '/lib'),
-    filename: 'index.js',
-    library: 'WebChat',
-    libraryTarget: 'umd'
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'string-replace-loader',
-            options: {
-              search: 'PACKAGE_VERSION_TO_BE_REPLACED',
-              replace: version
-            }
-          },
-          { loader: 'babel-loader' }
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-          {
-            loader: 'sass-loader',
-            options: {
-              includePaths: [path.resolve(__dirname, 'src/scss/')]
-            }
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(jpg|png|gif|svg|woff|ttf|eot)$/,
-        use: {
-          loader: 'url-loader'
-        }
-      }
-    ]
-  },
-  plugins: [new CleanWebpackPlugin(['lib'])]
-}, {
-  entry: './index.js',
+module.exports = {
+  entry: "./index.js",
   externals: {
     react: {
-      root: 'React',
-      commonjs2: 'react',
-      commonjs: 'react',
-      amd: 'react',
-      umd: 'react'
+      root: "React",
+      commonjs2: "react",
+      commonjs: "react",
+      amd: "react",
+      umd: "react"
     },
-    'react-dom': {
-      root: 'ReactDOM',
-      commonjs2: 'react-dom',
-      commonjs: 'react-dom',
-      amd: 'react-dom',
-      umd: 'react-dom'
+    "react-dom": {
+      root: "ReactDOM",
+      commonjs2: "react-dom",
+      commonjs: "react-dom",
+      amd: "react-dom",
+      umd: "react-dom"
     }
   },
   output: {
-    path: path.join(__dirname, '/module'),
-    filename: 'index.js',
-    library: 'WebChat',
-    libraryTarget: 'umd'
+    path: path.join(__dirname, "/lib"),
+    filename: "bundle.js",
+    library: "WebChat",
+    libraryTarget: "umd"
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"],
+    fallback: {
+      path: require.resolve("path-browserify")
+    }
   },
-  mode: 'production',
+  mode: "production",
   module: {
     rules: [
       {
@@ -95,40 +41,39 @@ module.exports = [{
         exclude: /node_modules/,
         use: [
           {
-            loader: 'string-replace-loader',
+            loader: "string-replace-loader",
             options: {
-              search: 'PACKAGE_VERSION_TO_BE_REPLACED',
+              search: "PACKAGE_VERSION_TO_BE_REPLACED",
               replace: version
             }
           },
-          { loader: 'babel-loader' }
+          { loader: "babel-loader" }
         ]
       },
       {
         test: /\.scss$/,
         use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
+          { loader: "style-loader" },
+          { loader: "css-loader" },
           {
-            loader: 'sass-loader',
+            loader: "sass-loader",
             options: {
-              includePaths: [path.resolve(__dirname, 'src/scss/')]
+              sassOptions: {
+                includePaths: [path.resolve(__dirname, "src/scss/")]
+              }
             }
           }
         ]
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"]
       },
       {
         test: /\.(jpg|png|gif|svg|woff|ttf|eot)$/,
-        use: {
-          loader: 'url-loader'
-        }
+        type: "asset/resource"
       }
     ]
   },
-  plugins: [new CleanWebpackPlugin(['module'])]
-}
-];
+  plugins: [new CleanWebpackPlugin(["lib"])]
+};
